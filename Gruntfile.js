84be87d95443425dc17e8933ebbf88d634dba9c6
Graham Licence <git@grahamlicence.co.uk>
2014-02-10 11:56:31 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+/*
+    Gruntfile. To run:
+    - install node
+    - run `npm install` in the root directory
+    - type in `grunt` to do run the build
+    - type in `grunt watch` whilst developing
+
+
+    Check out the registerTask statements at the bottom for an idea of
+    task grouping.
+*/
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        /*  Read the package.json file for config values.
+            package.json keeps devDependencies as well, which make it easy 
+            for us to track and install node dependencies 
+        */
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Compass uses config.rb automatically so we don't need to specify anything
+        compass: {
+            build: {}
+        },
+
+        /*  Copy files to production folder.
+        */
+        copy: {
+            main: {
+                files: [
+                // main files
+                {expand: true, flatten: true, src: ['src/index.html'], dest: 'build'},
+                // manifest
+                {expand: true, flatten: true, src: 'src/manifest.json', dest: 'build'},
+                // all assets
+                {expand: true, flatten: true, src: ['src/assets/**'], dest: 'build/assets/', filter: 'isFile'}
+                ]
+            }
+        },
+
+        /*  Uglify seems to be the industry standard for minification and obfuscation nowadays.
+        */
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
+            },
+            build: {
+                src: [
+                    'src/js/*'
+                ],
+                dest: 'src/assets/tab.min.js'
+            }
+        },
+
+        watch: {
+            css: {
+                files: 'src/sass/**/*.scss',
+                tasks: ['compass']
+            }
+        }
+    });
+
+    /*  Loading the grunt plugins.
+        If you're having problems loading any plugins, make sure you have the latest package.json file
+        and try running `npm install`.
+    */
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    /*  The default task runs when you just run `grunt`.
+        "js" and "css" tasks process their respective files. 
+    */
+    grunt.registerTask('css', ['compass']);
+    grunt.registerTask('js', ['uglify']);
+    grunt.registerTask('release', ['uglify', 'compass', 'copy']);
+
+    grunt.registerTask('default', ['css', 'js']);
+};
