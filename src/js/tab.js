0bb6640fa2e9e3cf88c972c402865a25cb5b97dd
Graham Licence <git@grahamlicence.co.uk>
2014-02-11 16:23:11 +0000

    Weather details added

--- src/js/tab.js
+++ src/js/tab.js
@@ -28,6 +28,7 @@ Tab.showTime = function () {
 };
 
 Tab.locationData = function () {
+    "use strict";
     var loc = JSON.parse(localStorage.getItem('location')),
         woeid = localStorage.getItem('woeid'),
         place = document.createElement('div'),
@@ -40,36 +41,53 @@ Tab.locationData = function () {
         place.innerHTML = loc.address_components[1].long_name + ', ' + loc.address_components[2].long_name;
     }
 
-    function weatherForecast () {
-        var reqW;
+    function displayWeather (data) {
+        var results = data.query.results.channel,
+            html;
+
         weather.className = 'weather-forecast';
         document.getElementsByTagName('body')[0].insertBefore(weather);
+        console.log(results)
+        html = '<p>Sunset: ' + results.astronomy.sunset + '</p>';
+        html += '<p>' + results.item.condition.temp + '&deg;' + results.units.temperature + ' ' + results.item.condition.text + '</p>';
+        html += '<ul class="forecast">';
+        // show weekly forecast
+        // weather codes http://developer.yahoo.com/weather/#codes
+        _.each(results.item.forecast, function(day) {
+          html +=   '<li title="' + day.date + '">' +
+                    '<p>' + day.day + '</p>' +
+                    '<p>High: ' + day.high + '&deg;' + results.units.temperature + '</p>' +
+                    '<p>Low:  ' + day.low +'&deg;' + results.units.temperature + '</p>' +
+                    '<p>' + day.text +'</p>';
+        });
+
+        weather.innerHTML = html;
+    }
+
+    function weatherForecast () {
+        var reqW;
 
         reqW = new XMLHttpRequest;
 
-        var hasC = '%20AND%20u%3D\'c\''; 
+        // possibly use this if switching between temp units
+        var hasC = '%20AND%20u%3D\'c\'';
         
-        reqW.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + 20089556 + hasC + '&format=json&diagnostics=true&callback=', true);
+        reqW.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + loc + hasC + '&format=json&diagnostics=true&callback=', true);
 
         reqW.onload = function() {
           if (reqW.status >= 200 && reqW.status < 400) {
-            // console.log(reqW.responseText)
-            // return;
             data = JSON.parse(reqW.responseText);
-            var results = data.query.results.channel;
-            console.log(data)
-            var html = '<p>Sunset: ' + results.astronomy.sunset + '</p>';
-
-            weather.innerHTML = html;
+            localStorage.setItem('weatherUpdate', Date.now());
+            localStorage.setItem('weatherData', JSON.stringify(data));
+            displayWeather(data)
           } else {
             // error
-            console.log('error status')
+            console.log('error on load')
           }
         };
 
         reqW.onerror = function() {
-            console.log('error')
-          // There was a connection error of some sort
+            console.log('connection error')
         };
 
         reqW.send();
@@ -77,11 +95,18 @@ Tab.locationData = function () {
     }
 
     locationName();
-    weatherForecast();
+    var lastChecked = parseInt(localStorage.getItem('weatherUpdate'), 10);
+    // check weather every four hours
+    if (Date.now() - lastChecked > 14400000) {
+        weatherForecast();
+    } else {
+        displayWeather(JSON.parse(localStorage.getItem('weatherData')));
+    }
 
 };
 
 Tab.getLocation = function () {
+    "use strict";
     if ('geolocation' in navigator === false) {
         return;
     }
@@ -90,21 +115,17 @@ Tab.getLocation = function () {
         return;
     } else {
         navigator.geolocation.getCurrentPosition(function(position) {
-            // do_something(position.coords.latitude, position.coords.longitude);
 
             request = new XMLHttpRequest;
             request.open('GET', 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=true', true);
 
 
             request.onload = function() {
-              if (request.status >= 200 && request.status < 400){
-                // Success!
+              if (request.status >= 200 && request.status < 400) {
                 data = JSON.parse(request.responseText);
-                console.log(data)
                 var town = _.find(data.results, function (ac) { return ac.types[0] == 'postal_code' });
                 localStorage.setItem('location', JSON.stringify(town));
                 // get WOEID
-                //http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22Canary%20Wharf%20London%22&format=json
                 Tab.getWoeid(town.address_components[1].long_name.replace(/ /g, '%20') + '%20' + town.address_components[town.address_components.length - 1].long_name);
               } else {
                 // error
@@ -119,41 +140,27 @@ Tab.getLocation = function () {
 
             request.send();
 
-            // just use yahoo??
+            // TODO: just use yahoo??
             //http://where.yahooapis.com/geocode?location=37.42,-122.12&flags=J&gflags=R&appid=zHgnBS4m
         });
     }
 };
 
-Tab.init = function  () {
-    Tab.showTime();
-    Tab.getLocation();
-};
-
-Tab.init();
-
 Tab.getWoeid = function (place) {
+    "use strict";
     var request;
     navigator.geolocation.getCurrentPosition(function(position) {
 
         request = new XMLHttpRequest;
-                    //
-        // request.open('GET', 'http://where.yahooapis.com/geocode?location=' + position.coords.latitude + ',' + position.coords.longitude + '&flags=J&gflags=R&appid=zHgnBS4m', true);
-        request.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22' + place + '%22&format=json', true);
 
+        request.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22' + place + '%22&format=json', true);
 
         request.onload = function() {
           if (request.status >= 200 && request.status < 400){
-            // Success!
             data = JSON.parse(request.responseText);
-            console.log(data.query.results.place[0].woeid)
-            // var town = _.find(data.results, function (ac) { return ac.types[0] == 'postal_code' });
-            // localStorage.setItem('location', JSON.stringify(town));
-            // get WOEID
-            //http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22Canary%20Wharf%20London%22&format=xml
-            // Tab.locationData();
+            // console.log(data.query.results.place[0].woeid)
             localStorage.setItem('woeid', data.query.results.place[0].woeid);
-                Tab.locationData();
+            Tab.locationData();
 
           } else {
             // error
@@ -171,39 +178,9 @@ Tab.getWoeid = function (place) {
     });
 };
 
-// this is prolly fine in chrome
- function weatherForecast () {
-        var reqW;
+Tab.init = function  () {
+    Tab.showTime();
+    Tab.getLocation();
+};
 
-        reqW = new XMLHttpRequest;
-                    //
-
-        // reqW.open('GET', 'http://weather.yahooapis.com/forecastrss?w=' + 20089556 + '&u=c', true);
-        var hasC = '%20AND%20u%3D\'c\''
-        
-        reqW.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + 20089556 + hasC + '&format=json&diagnostics=true&callback=', true);
-
-
-        reqW.onload = function() {
-          if (reqW.status >= 200 && reqW.status < 400) {
-            // console.log(reqW.responseText)
-            // return;
-            data = JSON.parse(reqW.responseText);
-            console.log(data)
-
-          } else {
-            // error
-            console.log('error status')
-          }
-        };
-
-        reqW.onerror = function() {
-            console.log('error')
-          // There was a connection error of some sort
-        };
-
-        reqW.send();
-            
-    }
-
-    // weatherForecast();
+Tab.init();
